56687f5da1ff1129129c42116a64db1205c96136
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-02-07 20:05:06 -0500

    Initialize project, set up router, create db

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [fighterList, setFighterList] = useState([])
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/fighters')
+    .then(r => r.json())
+    .then(data => setFighterList(data))
+  }, [])
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Here I am</h1>
+    {fighterList.map((fighter) => {
+      return <p key={fighter.id}>{fighter.name}</p>
+    })}
+    </>
   );
 }
 
