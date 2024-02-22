98010ad5b4c311aa2c4544df01aa61bd81d75b52
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-02-21 21:13:12 -0500

    Create CSS for WorkoutCard

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -2,14 +2,14 @@
 
 function WorkoutCard({workout}) {
     const {id, type, length, location, notes, imgUrl} = workout
+    
     return (
-        <>
+        <div className='card'>
             <h2>{type}</h2>
             <h2>{length}</h2>
             <h2>{location}</h2>
             <p>{notes}</p>
-
-        </>
+        </div>
     )
 }
 
