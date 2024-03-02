e2147f9ab777c3cb5c763e844febcea10817f4f4
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-03-01 22:55:16 -0500

    Add text to About component

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,17 @@
 
 function About() {
     return (
-        <h1>About</h1>
+        <>
+            <h1>About</h1>
+            <p>
+                This is a personal workout logging app.
+                Although simple, it's important to keep track of one's progress.
+                The ability to look back and see all of your workouts at a glance
+                can be a powerful tool for motivation. Don't forget that everyone
+                has to start somewhere! Start logging your workouts today and see
+                how long you can keep it up!
+            </p>
+        </>
     )
 }
 
